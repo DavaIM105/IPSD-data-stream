14cdccd670bb981b9ea54cffc4b7b6026174c030
DavaIM105 <l200220105@student.ums.ac.id>
2024-12-08 02:20:31 +0700

    penyesuaian

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from google.cloud import bigquery
 def etl_pipeline():
     try:
         # Extract: Ambil data Yahoo Finance
+        # symbols = "BBCA.JK"
         symbols = ["BBCA.JK", "TLKM.JK", "UNVR.JK"]
         data = yf.download(tickers=symbols, period="5d", interval="1m")
         data.to_csv("latest_data_saham.csv")
-    
         
         # Transform: Bersihkan data
         old_data_name = "data_saham.csv"
@@ -20,8 +20,11 @@ def etl_pipeline():
             old_data = pd.read_csv(old_data_name, index_col=0, header=[0,1])
             data.to_csv(old_data_name)
             new_data = data[~data.index.isin(old_data.index.astype(data.index.dtype))]
+            new_data.to_csv(latest_data_name)
+        else:
+            data.to_csv(old_data_name)
+            data.to_csv(latest_data_name)
             
-        new_data.to_csv(latest_data_name)
             
         print(f"Data berhasil disimpan ke {latest_data_name}")
             
@@ -30,18 +33,19 @@ def etl_pipeline():
         df.dropna(inplace=True)
         df.drop_duplicates(inplace=True)
         
+        df.index = pd.to_datetime(df.index)
         # Flatten multi-index kolom
         df.columns = ['_'.join(map(str, col)) for col in df.columns]
         # Ganti karakter yang tidak valid pada nama kolom
         df.columns = [col.replace('.', '-') for col in df.columns]
-        df.index = pd.to_datetime(df.index)
         
         df.to_csv(latest_data_name)
-    
+
         
         # Load: Muat ke BigQuery
         client = bigquery.Client()
-        table_id = "data-stream-spread.dataset_stream_saham.transformed_stream_saham"
+        table_id = "data-stream-spread.dataset_stream_saham.data-saham"
+        # table_id = "data-stream-spread.stream_saham.saham-bca"
         job_config = bigquery.LoadJobConfig(
             autodetect=True,
             write_disposition=bigquery.WriteDisposition.WRITE_APPEND
